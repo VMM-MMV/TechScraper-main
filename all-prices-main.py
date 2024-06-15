dd34cea0e3494d12ee9fbb41c7cb21a93de150cc
VMM-MMV <121231490+VMM-MMV@users.noreply.github.com>
2024-06-15 23:01:17 +0000

    put to json

--- all-prices-main.py
+++ all-prices-main.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import json
 
 base_url = "https://www.allprices.md"
 page_number = 1
 total_links = 0
 
-def get_shops_and_price(url):
+def get_smallest_price_and_shop(url):
     response = requests.get(url)
     html_content = response.content
 
@@ -18,18 +19,20 @@ def get_shops_and_price(url):
     # Find all table rows within the product prices table
     product_rows = product_table.find_all('tr')
 
-    shops = {}
     # Loop through each row and extract the shop name and price
+    price_mdl = float('inf')
+    shop_name = ""
     for row in product_rows:
         try:
-            shop_name = row.find('td', class_='col-shop bold').find('a').text
-            price_mdl = row.find('div', class_='price price-mdl').find('span', class_='value').text
-            shops[shop_name] = price_mdl
+            temp_price = row.find('div', class_='price price-mdl').find('span', class_='value').text
+            if temp_price < price_mdl:
+                shop_name = row.find('td', class_='col-shop bold').find('a').text
+                price_mdl = row.find('div', class_='price price-mdl').find('span', class_='value').text
         except:
             pass
 
     time.sleep(1)
-    return shops
+    return shop_name, price_mdl
 
 def get_pc_specs(url):
     response = requests.get(url)
@@ -58,6 +61,7 @@ def get_pc_specs(url):
     time.sleep(1)
     return specs
 
+all_results = []
 while True:
     # Construct URL for the current page
     url = f"{base_url}/ro/produse/calculatoare/laptop-uri?page={page_number}"
@@ -68,15 +72,17 @@ while True:
     
     # Parse the HTML
     soup = BeautifulSoup(html_content, 'html.parser')
-    
+
     # Find all <a> tags on the page
     links = soup.find_all('a', class_='thumbnail')
     for link in links:
+        name = link.replace("/ro/produse/calculatoare/laptop-uri/", "").replace("-", " ")
         clean_link = link.get('href')
-        shops = get_shops_and_price(f"{base_url}/{clean_link}/preturi")
+        shop, price = get_smallest_price_and_shop(f"{base_url}/{clean_link}/preturi")
         specs = get_pc_specs(f"{base_url}/{clean_link}/specificatii")
-        print(specs)
-        print(shops)
+        result = {"name": name, "price": price, 'additional_specs': specs, "store_name": shop}
+        print(result)
+        all_results.append(result)
 
     # Check if there are any links on the page
     if len(links) == 0 or total_links == 100:
@@ -96,3 +102,5 @@ while True:
     time.sleep(1)
 
 print(f"Total links found: {total_links}")
+with open('results_all.json', 'w', encoding='utf-8') as f:
+    json.dump(all_results, f, ensure_ascii=False, indent=4)
